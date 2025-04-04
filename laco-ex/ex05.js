d084bd2ce20d17db8d3c8e49b87d642972c76104
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-04-04 09:29:54 -0300

    ex05

---
+++ laco-ex/ex05.js
@@ -0,0 +1,8 @@
+//Calcular o quadrado dos números de 1 a 10
+for (let i = 1; i <=10; i++) {
+    console.log(`${i}² = ${i * i}`);
+}
+
+for (let i = 1; i<=10; i++) {
+    console.log(`${i}ˆ3 = ${Math.pow(i, 3)}`);
+}
