d4749f98870264af1044b03b51b9103a0d1d6513
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-04-04 08:49:45 -0300

    exercicio 01

---
+++ laco-ex/ex01.js
@@ -0,0 +1,7 @@
+git init
+git add . -> all ( todos os arquivos)
+git add ex01.js -> specific ( arquivo específico)
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/thiago-rferreira/lista-for-exemplo.git
+git push -u origin main
